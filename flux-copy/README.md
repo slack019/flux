2cb5aae202e69839038d0dc663da04604bdc9c78
Kirill Tepikin <kirillbursaev@yandex.ru>
2025-06-13 05:26:26 +0300

    Added tests

---
+++ flux-copy/README.md
@@ -0,0 +1,59 @@
+<h1 align ="center"> 🔨 Flux </h1> 
+
+**C++ 20 modern utils and containers library**
+
+
+<h2 align="center"> Installation </h2> 
+
+**Use git for install**
+
+```bash 
+git clone --depth 1 https://github.com/slack019/flux.git flux/ 
+```
+
+<h2 align="center"> Build  </h2>
+
+**You can use install.sh script or build independently**
+
+**CMake Build Options:**
+
+`-DFLUX_SHARED_LIBS` - if you want to build shared library (ON/OFF)
+
+`-DBUILD_EXAMPLES` - if you want to build library examples(ON/OFF)
+
+`-DBUILD_TESTS` - if you want to build library tests(ON/OFF)
+
+`-DCMAKE_BUILD_TYPE` - Build type (Debug, Release)
+
+`-DFLUX_INSTALL` - Generate target for installing flux (ON/OFF)
+
+```bash
+ 
+mkdir build 
+cd build 
+sudo cmake -S .. -B . -DFLUX_INSTALL=ON -DCMAKE_INSTALL_PREFIX=/usr/local 
+cmake --build . --target install 
+
+```
+
+<h2 align="center"> Usage </h2>
+
+```cpp
+
+#include <iostream> // for std::cout
+
+#include <flux/containers/string.hpp>
+
+auto main() -> int { 
+    fl::containers::string str {"Hello"};
+
+    std::cout << str + " World!" << std::endl;
+    return 0; 
+} 
+
+```
+
+
+<h2 align="center"> License </h2>
+
+Project have [MIT](https://opensource.org/license/mit) license  
---
+++ flux-copy/include/flux/containers/string.hpp
@@ -0,0 +1,82 @@
+// MIT License
+// 
+// Copyright (c) 2025 @slack019
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef FLUX_STRING_ASSET_HPP
+#define FLUX_STRING_ASSET_HPP
+
+#define FLUX_STATIC_DEFINE 1
+
+#include <initializer_list> // for std::initializer_list
+#include <algorithm> // for std::min, std::max
+#include <utility> // for std::swap, std::move
+#include <compare> // for std::strong_ordering
+#include <iostream>
+
+#include <cstddef> 
+#include <cstring> 
+
+#include <flux/export-flux.hpp>
+
+namespace fl::containers { 
+  
+  class string final { 
+  private: 
+    char* m_ascii_string;
+    size_t m_size;
+    size_t m_capacity;
+  public: 
+    string(); 
+    string(const char*);
+    string(const string&);
+    string(std::initializer_list<char>);
+    ~string();
+  public: 
+    size_t length() const; 
+    size_t capacity() const; 
+    char& front();
+    const char& front() const;
+    char& back();
+    const char& back() const;
+    char& at(const size_t&);
+    const char& at(const size_t&) const;
+    void clear();
+    void shrink_to_fit();
+    void resize(const size_t&, char = 0);
+    void reserve(const size_t& = 0);
+    bool empty() const;
+  public: 
+    friend std::ostream& operator<<(std::ostream&, const string&);
+    friend std::istream& operator>>(std::istream&, string&);
+    friend string operator+(const string&, const string&);
+    friend string operator+(const string&, const char*); 
+    string& operator+=(const string&); 
+    string& operator+=(const char*);
+    string& operator=(const string&) &;
+    char& operator[](const size_t&);
+    const char& operator[](const size_t&) const;
+    bool operator==(const string&) const;
+    bool operator!=(const string&) const;
+  };
+  
+} // fl::containers 
+
+#endif
